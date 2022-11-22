3b1781e68ed512360f350a0ebcc98bd9ec683da4
Demetris Tzionis <demetristz@hotmail.com>
2022-11-22 19:07:15 +0000

    updated README and fixed goal score to 100

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # first-to-100
-A game made using JS as part of Jonas' "The Complete JavaScript Course 2023"
+
+Web game generated with JavaScript, HTML and CSS, uploaded by Demetris Tzionis as part of Jona's Udemy course (https://www.udemy.com/course/the-complete-javascript-course/)
+
+PROJECT NAME First to 100
+
+INSTRUCTIONS 
+
+This is a fun 2-player game built as practice for using JavaScript to manipulate the DOM. 
+
+Keep rolling the dice to sum up the score BUT rolling 1 resets the score and the game switches to the other player. Press HOLD if you want to save your score and give the turn to the other player. First to 100 wins. 
+
+INSTALLATION Download the code files and open index.html in your browser or run a local server
+
+TECH STACK
+
+    JavaScript
+    HTML
+    CSS
+
+
--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-'use strict';
+"use strict";
 
 // Selecting elements
-const score0El = document.getElementById('score--0');
-const score1El = document.getElementById('score--1');
-const current0El = document.getElementById('current--0');
-const current1El = document.getElementById('current--1');
-const player0El = document.querySelector('.player--0');
-const player1El = document.querySelector('.player--1');
+const score0El = document.getElementById("score--0");
+const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 
-const diceEl = document.querySelector('.dice');
-const btnNew = document.querySelector('.btn--new');
-const btnRoll = document.querySelector('.btn--roll');
-const btnHold = document.querySelector('.btn--hold');
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
 
 // Declare these outside init() for scope reasons
 let scores, currentScore, activePlayer, playing;
@@ -27,11 +27,11 @@ const init = function () {
   current0El.textContent = 0;
   current1El.textContent = 0;
 
-  diceEl.classList.add('hidden');
-  player0El.classList.remove('player--winner');
-  player1El.classList.remove('player--winner');
-  player0El.classList.add('player--active');
-  player1El.classList.remove('player--active');
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
 };
 
 // Run init so that when the page loads everything is reset
@@ -41,12 +41,12 @@ const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   currentScore = 0;
   activePlayer = activePlayer === 0 ? 1 : 0; // Switch to next player
-  player0El.classList.toggle('player--active');
-  player1El.classList.toggle('player--active');
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
 };
 
 // Rolling dice functionality
-btnRoll.addEventListener('click', function () {
+btnRoll.addEventListener("click", function () {
   if (playing) {
     // 1. Generating random dice number
     const dice = Math.trunc(Math.random() * 6) + 1;
@@ -54,7 +54,7 @@ btnRoll.addEventListener('click', function () {
 
     // 2. Display dice
 
-    diceEl.classList.remove('hidden');
+    diceEl.classList.remove("hidden");
     diceEl.src = `dice-${dice}.png`;
 
     // 3. Check for rolled 1
@@ -69,7 +69,7 @@ btnRoll.addEventListener('click', function () {
   }
 });
 
-btnHold.addEventListener('click', function () {
+btnHold.addEventListener("click", function () {
   if (playing) {
     // 1. Add current score to active player's score
     scores[activePlayer] += currentScore;
@@ -78,9 +78,9 @@ btnHold.addEventListener('click', function () {
 
     // 2. Check if player's score is >= 100
     // Finish the game
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
-      diceEl.classList.add('hidden');
+      diceEl.classList.add("hidden");
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.add(`player--winner`);
@@ -94,4 +94,4 @@ btnHold.addEventListener('click', function () {
   }
 });
 
-btnNew.addEventListener('click', init);
+btnNew.addEventListener("click", init);
